64c51a88a549bd009c69c7c240070fb50b0effbc
Gary Houston <ghouston@arglist.com>
2017-04-11 16:31:55 +1000

    clean up, not markdown format

--- doc.go
+++ doc.go
@@ -5,9 +5,11 @@ or modify metadata in formats such as Exif and XMP.
 
 Example programs are in the repository:
 
-* jpegsegsprint prints the markers and segment lengths for a JPEG file, up to th start of scan (SOS) marker.
-* jpegsegscopy makes an unmodified copy of a JPEG file.
-* jpegsegsstrip makes a copy of a JPEG file with all COM, APP and JPG segments removed.
+jpegsegsprint prints the markers and segment lengths for a JPEG file, up to th start of scan (SOS) marker.
+
+jpegsegscopy makes an unmodified copy of a JPEG file.
+
+jpegsegsstrip makes a copy of a JPEG file with all COM, APP and JPG segments removed.
 
 */
 package jpegsegs
