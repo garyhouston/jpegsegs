47b04c60f2889f6ae64aaa37c4a032d187c05d27
Gary Houston <ghouston@arglist.com>
2017-05-14 15:41:36 +1000

    Initial attempt at supporting MPF files.

--- jpegsegscopy/jpegsegscopy.go
+++ jpegsegscopy/jpegsegscopy.go
@@ -1,14 +1,163 @@
 package main
 
+// Unpack a JPEG file one segment at a time and repackage into a new
+// JPEG file.  It can process files using the Multi-Picture Format
+// extension to contain multiple images.
+
 import (
 	"fmt"
-	"io"
 	jseg "github.com/garyhouston/jpegsegs"
+	tiff "github.com/garyhouston/tiff66"
+	"io"
 	"log"
 	"os"
 )
 
-// Copy a JPEG file one segment at a time without modifying it.
+// Serialize a MPF TIFF tree into a new APP2 segment.
+func putMPF(mpfTree *tiff.IFDNode) ([]byte, error) {
+	size := jseg.MPFHeaderSize + tiff.HeaderSize + mpfTree.TreeSize()
+	newbuf := make([]byte, size)
+	next := jseg.PutMPFHeader(newbuf)
+	tiff.PutHeader(newbuf[next:], mpfTree.Order, tiff.HeaderSize)
+	if _, err := mpfTree.PutIFDTree(newbuf[next:], tiff.HeaderSize); err != nil {
+		return nil, err
+	}
+	return newbuf, nil
+}
+
+// Copy a single image. If a MPF segment is found in APP2, return a
+// MPF TIFF tree, the MPF image offsets in the input file, and the
+// position where the MPF segment was written in the output file.
+func copyImage(writer io.WriteSeeker, reader io.ReadSeeker, mpfSpace tiff.TagSpace) (*tiff.IFDNode, []uint32, uint32, error) {
+	scanner, err := jseg.NewScanner(reader)
+	if err != nil {
+		return nil, nil, 0, err
+	}
+	dumper, err := jseg.NewDumper(writer)
+	if err != nil {
+		return nil, nil, 0, err
+	}
+	var mpfTree *tiff.IFDNode
+	var offsets []uint32
+	var mpfWritePos uint32
+	for {
+		marker, buf, err := scanner.Scan()
+		if err != nil {
+			return nil, nil, 0, err
+		}
+		if marker == jseg.APP0+2 {
+			isMPF, next := jseg.GetMPFHeader(buf)
+			if isMPF {
+				// Copy buf so that it can be used outside this loop via pointers in the TIFF tree.
+				savebuf := make([]byte, len(buf)-jseg.MPFHeaderSize)
+				copy(savebuf, buf[next:])
+				mpfTree, err = jseg.GetMPFTree(savebuf, mpfSpace)
+				if err != nil {
+					return nil, nil, 0, err
+				}
+				mpfTree.Fix()
+				if mpfTree.Space == tiff.MPFIndexSpace {
+					// MPF offsets are relative to
+					// the start of the MPF
+					// header, which is 4 bytes
+					// past the start of buf.  The
+					// current position of the
+					// reader is one byte past the
+					// data read into buf.
+					pos, err := reader.Seek(0, io.SeekCurrent)
+					if err != nil {
+						return nil, nil, 0, err
+					}
+					mpfOffset := uint32(pos) - uint32(len(buf)-4)
+					offsets, err = jseg.MPFImageOffsets(mpfTree, mpfOffset)
+					if err != nil {
+						return nil, nil, 0, err
+					}
+				}
+				buf, err = putMPF(mpfTree)
+				if err != nil {
+					return nil, nil, 0, err
+				}
+				pos, err := writer.Seek(0, io.SeekCurrent)
+				if err != nil {
+					return nil, nil, 0, err
+				}
+				mpfWritePos = uint32(pos)
+			}
+		}
+		if err := dumper.Dump(marker, buf); err != nil {
+			return nil, nil, 0, err
+		}
+		if marker == jseg.EOI {
+			break
+		}
+	}
+	return mpfTree, offsets, mpfWritePos, nil
+}
+
+// Copy additional images specified with MPF.
+func copyMPFImages(writer io.WriteSeeker, reader io.ReadSeeker, offsets []uint32) ([]uint32, error) {
+	count := uint32(len(offsets))
+	newOffsets := make([]uint32, len(offsets))
+	for i := uint32(0); i < count; i++ {
+		if offsets[i] > 0 {
+			if _, err := reader.Seek(int64(offsets[i]), io.SeekStart); err != nil {
+				return nil, err
+			}
+			pos, err := writer.Seek(0, io.SeekCurrent)
+			if err != nil {
+				return nil, err
+			}
+			newOffsets[i] = uint32(pos)
+			if _, _, _, err := copyImage(writer, reader, tiff.MPFAttributeSpace); err != nil {
+				return nil, err
+			}
+			pos, err = writer.Seek(0, io.SeekCurrent)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return newOffsets, nil
+}
+
+// Modify a MPF TIFF tree with new image offsets and sizes, then overwrite the
+// MPF data in the output stream.
+func rewriteMPF(writer io.WriteSeeker, mpfTree *tiff.IFDNode, mpfWritePos uint32, offsets, lengths []uint32) error {
+	order := mpfTree.Order
+	for _, f := range mpfTree.Fields {
+		if f.Tag == jseg.MPFEntry {
+			for i := 0; i < len(offsets); i++ {
+				var offset uint32
+				if offsets[i] > 0 {
+					// MPF offsets are relative to
+					// the byte after the MPF
+					// header, i.e., 8 bytes into
+					// the MPF APP2 block.
+					offset = offsets[i] - mpfWritePos - 8
+				}
+				f.PutLong(lengths[i], uint32(i*4+1), order)
+				f.PutLong(offset, uint32(i*4+2), order)
+				// too far in by 3 bytes ?
+			}
+		}
+	}
+	newbuf, err := putMPF(mpfTree)
+	if err != nil {
+		return err
+	}
+	if _, err := writer.Seek(int64(mpfWritePos), io.SeekStart); err != nil {
+		return err
+	}
+	if err := jseg.WriteMarker(writer, jseg.APP0+2); err != nil {
+		return err
+	}
+	if err := jseg.WriteData(writer, newbuf); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Printf("Usage: %s infile outfile\n", os.Args[0])
@@ -19,35 +168,33 @@ func main() {
 		log.Fatal(err)
 	}
 	defer reader.Close()
-	scanner, err := jseg.NewScanner(reader)
-	if err != nil {
-		log.Fatal(err)
-	}
 	writer, err := os.Create(os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer writer.Close()
-	dumper, err := jseg.NewDumper(writer)
+	mpfTree, imageOffsets, mpfWritePos, err := copyImage(writer, reader, tiff.MPFIndexSpace)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for {
-		marker, buf, err := scanner.Scan()
+	if mpfTree != nil && mpfTree.Space == tiff.MPFIndexSpace {
+		newOffsets, err := copyMPFImages(writer, reader, imageOffsets)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if err := dumper.Dump(marker, buf); err != nil {
+		numImages := len(newOffsets)
+		newLengths := make([]uint32, numImages)
+		for i := 0; i < numImages-1; i++ {
+			newLengths[i] = newOffsets[i+1] - newOffsets[i]
+		}
+		lastpos, err := writer.Seek(0, io.SeekCurrent)
+		if err != nil {
 			log.Fatal(err)
 		}
-		if marker == jseg.EOI {
-			break
+		newLengths[numImages-1] = uint32(lastpos) - newOffsets[numImages-1]
+		if err = rewriteMPF(writer, mpfTree, mpfWritePos, newOffsets, newLengths); err != nil {
+			log.Fatal(err)
 		}
-		
-	}
-	// There may be more images after the EOI marker if the file is
-	// using Multi-Picture Format. Just copy it for now.
-	if _, err := io.Copy(writer, reader); err != nil {
-		log.Fatal(err)
+
 	}
 }
