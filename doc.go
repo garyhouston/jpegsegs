c393e463bdab6fe17e35818edf528ee785f84b28
Gary Houston <ghouston@arglist.com>
2017-06-04 16:38:39 +1000

    Fix a typo

--- doc.go
+++ doc.go
@@ -7,6 +7,6 @@ Reading and writing JPEG segments can be done by creating "scanners" and "dumper
 
 Processing files that use MPF is more complex. The MPF information is stored in APP2 segments in TIFF format; the MPF segment in the first file starts with index information. The index gives the offsets and lengths of the individual images. Reading the images can be done by unpacking the MPF index and seeking the input stream to each image in turn. This is demonstrated by the jpegsegsprint program.
 
-Writing a multi-image file with MPF requires that the file positions of all images be encoded into the MPF index. The approach taken here is to initially write the index into the first image with nominal values, to reserve the appropriate amount of space in the APP2 segment. After all images have been writen to the output, and the positions collected, the APP2 segment is then rewritten with the final positions. This is demonstrated by the jpegsegscopy program.
+Writing a multi-image file with MPF requires that the file positions of all images be encoded into the MPF index. The approach taken here is to initially write the index into the first image with nominal values, to reserve the appropriate amount of space in the APP2 segment. After all images have been written to the output, and the positions collected, the APP2 segment is then rewritten with the final positions. This is demonstrated by the jpegsegscopy program.
 */
 package jpegsegs
